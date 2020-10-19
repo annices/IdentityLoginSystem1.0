bb402a9d98ddca9453250ed63c74b903a507fce9
Annice Strömberg <annice.stromberg@gmail.com>
2020-10-19 22:37:14 +0200

    Update RoleController.cs

--- LoginSystem/Controllers/RoleController.cs
+++ LoginSystem/Controllers/RoleController.cs
@@ -7,6 +7,17 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 
+/*
+Copyright © 2020 Annice Strömberg – Annice.se
+This script is MIT (Massachusetts Institute of Technology) licensed, which means that
+permission is granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files to deal in the software without restriction. This
+includes, without limitation, the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the software, and to permit persons to whom the software
+is furnished to do so subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the software.
+*/
 namespace LoginSystem.Controllers
 {
     /// <summary>
